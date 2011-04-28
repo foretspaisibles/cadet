540de3c342e42759adb05a20771d75c20f1433e8
Michael Grünewald <michipili@gmail.com>
2011-04-28 12:24:52 +0000

    Include new workflow for the website

---
+++ Library/Make/bhrid.scroll.mk
@@ -0,0 +1,25 @@
+
+WWWMAIN?= main.sgml
+
+SRCS+= main.sgml
+SRCS+= head-css-global.sgml
+SRCS+= more-news.sgml
+SRCS+= copyright-statement.sgml
+
+SEARCH+= .
+SEARCH+= ${PROJECTBASE}/Website/style
+SEARCH+= ${PROJECTBASE}/Website/sgml
+
+SCROLL_INCLUSION = head-css-local
+SCROLL_INCLUSION+= more-news
+SCROLL_INCLUSION+= more-ilink
+SCROLL_INCLUSION+= more-elink
+SCROLL_INCLUSION+= more-download
+
+.for inclusion in ${SCROLL_INCLUSION}
+.if !empty(SRCS:M${inclusion}.sgml)
+INCLUDE+= with.${inclusion}
+.endif
+.endfor
+
+.include "www.sgml.mk"
